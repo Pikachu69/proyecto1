87290c0a5cd7b3e6c3a2bafb6a4c5c4c612a25b0
pikachu69 <lalitro.es@gmail.com>
2016-02-17 11:08:41 -0600

    Ventana creada

--- ventana.java
+++ ventana.java
@@ -1,2 +1,29 @@
+//# proyecto1
 import javax.swing.*;
 import java.awt.*;
+
+class ventana extends JFrame{
+ JLabel user = new JLabel("User: ");
+ JTextField userText = new JTextField(15);
+ JLabel pass = new JLabel("Password: ");
+ JPasswordField passField = new JPasswordField(15);
+ JButton cancel = new JButton("Cancel");
+ JButton ok = new JButton("Ok");
+
+ventana(){
+this.setLayout(new FlowLayout());
+	add(user);
+	add(userText);
+	add(pass);
+	add(passField);
+	add(cancel);
+	add(ok);
+setVisible(true);
+setSize(300,300);
+setTitle("Ventana del Proyecto 1");
+setDefaultCloseOperation(EXIT_ON_CLOSE);
+}
+public static void main(String ar[]){
+new ventana();
+}
+}
